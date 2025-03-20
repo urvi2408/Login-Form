5944912644d52a7775abd3d7673dd9f915a858d5
urvi2408 <bhavsarurvi143@gmail.com>
2025-03-20 11:39:16 +0530

    change in initial state of counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,35 @@
-import React, {useState} from "react";
-import './Counter.css';
+import React, { useState } from "react";
+import "./Counter.css";
 
 function Count() {
+  const [counter, setNum] = useState(0);
 
-    const [counter, setNum] = useState("");
+  const IncNum = () => {
+    setNum(Number(counter) + 1);
+  };
 
-    const IncNum = () => {
-        setNum(Number(counter) + 1);
-    }
+  const DecNum = () => {
+    setNum(counter - 1);
+  };
 
-    const DecNum = () => {
-        setNum(counter - 1);
-    }
+  const Reset = () => {
+    setNum(0);
+  };
 
-    const Reset = () =>{
-        setNum(0);
-    }
+  return (
+    <div className="count">
+      <input
+        className="num"
+        value={counter}
+        onChange={(e) => setNum(e.target.value)}
+      ></input>
+      <br />
+      <br />
+      <button onClick={IncNum}>Increment</button>
+      <button onClick={DecNum}>Decrement</button>
+      <button onClick={Reset}>Reset</button>
+    </div>
+  );
+}
 
-    return (
-       <div className="count">
-       <input className="num" value={counter} onChange={(e) => setNum(e.target.value)}></input><br/><br/>
-       <button onClick={IncNum}>Increment</button>
-       <button onClick={DecNum}>Decrement</button>
-       <button onClick={Reset}>Reset</button>
-       </div>
-    );
-
-} 
-
-export  default Count;
+export default Count;
